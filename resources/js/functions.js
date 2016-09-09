ae4ddd93d57f664edb0a956f927caba4718b823a
JuanIgnacioCuiule <juanignaciocuiule@gmail.com>
2016-09-09 11:55:03 -0300

    adding github and linkedin icons

--- resources/js/functions.js
+++ resources/js/functions.js
@@ -8,14 +8,15 @@ var revealed = true;
 $(window).scroll(function(){
   var wScroll = $(this).scrollTop();
   console.log(wScroll);
-
-	if (wScroll > 300 && revealed) {
-		b.start();
+	if (wScroll > $(window).height() && revealed) {
+		b.once();
+		console.log("Ocultar");
 		revealed = false;
 	}
 
-	if (wScroll < 300 && !revealed) {
-		b.reveal(500)
+	if (wScroll < $(window).height() && !revealed) {
+		b.reveal(500);
+		console.log("Reveal");
 		revealed = true;
 	}
 
